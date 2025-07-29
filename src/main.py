a35fdaffbcf3da59f2daf7b9d6bd24e1d0e349c8
virean196 <gama.bruno.196@gmail.com>
2025-07-29 20:15:45 +0100

    adding func split_nodes_delimiter

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, ParentNode, LeafNode
+from split_delimiter import split_nodes_delimiter
 
 text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
 html_node = HTMLNode(None, None, None, {
@@ -7,7 +8,7 @@ html_node = HTMLNode(None, None, None, {
     "target": "_blank",
 })
 
-print(html_node.props_to_html())
+#print(html_node.props_to_html())
 
 node = ParentNode(
     "p",
@@ -19,4 +20,11 @@ node = ParentNode(
     ],
 )
 
-print(node.to_html())
+code_node = [TextNode("`This` is a code node", TextType.CODE)]
+
+#print(node.to_html())
+
+test = split_nodes_delimiter(code_node, '`', TextType.CODE)
+
+print(test)
+
